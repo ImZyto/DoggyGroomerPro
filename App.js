d69fe6785654c65d3ec1f7e991bfdd3a0954ddec
Michal Zybura <michalzybura96@gmail.com>
2023-09-25 22:44:26 +0200

    clean project, init expo

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import Navigation from './src/navigation/Navigation';
-
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
